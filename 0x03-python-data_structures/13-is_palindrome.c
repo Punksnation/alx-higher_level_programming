90d3fe83e718346a695e02dc9ce7527946d7384d
Punksnation <cyberpunk26@gmail.com>
2023-10-09 06:30:33 -0700

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,56 +1,30 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * reverse_array - reverses the content of an array of integers
- * @a: int array to reverse
- * @n: number of elements in the array
- * Return: concatenated string
+ * recurse - recursively checks if a linked list is a palindrome
+ * @start: node to compare to end
+ * @end: node to compare to start
+ * Return: Palindrome(1) or Not(0)
  */
-
-void reverse_array(int *a, int n)
+int recurse(listint_t **start, listint_t *end)
 {
-        int *begin = a;
-        int *end;
-        int hold = 0;
+	int result;
 
-        end = a + n - 1;
-        for (; begin < end; begin++, end--)
-        {
-                hold = *end;
-                *end = *begin;
-                *begin = hold;
-        }
+	if (!end)
+		return (1);
+	result = recurse(start, end->next) && ((*start)->n == end->n);
+	*start = (*start)->next;
+	return result;
 }
 
 /**
- * is_palindrome - Return 1  if palindrome, 0 if not
- * @head: linked list
- * Return: Return 1  if palindrome, 0 if not
+ * is_palindrome - Checks if a linked list is a palindrome
+ * @head: pointer to the head of a linked list
+ * Return: Palindrome(1) or Not(0)
  */
-
 int is_palindrome(listint_t **head)
 {
-        int size, *list, *rev;
-        listint_t *copy = *head;
+	listint_t *node = *head;
 
-        if (!head || !copy)
-                return (0);
-        if (!copy->next)
-                return (1);
-
-        list = malloc(sizeof(int *));
-        if (!list)
-                return (0);
-        rev = malloc(sizeof(int *));
-        if (!rev)
-                return (0);
-        for (size = 0; copy; copy = copy->next, size++)
-                list[size] = copy->n;
-
-        list = rev;
-        reverse_array(rev, size);
-        if (list == rev)
-                return (1);
-        return (0);
+	return (recurse(&node, node));
 }
